8650c77ef7d84b5a784326bb26e242801093a833
adityab1179 <adityabansal1179@gmail.com>
2024-10-22 16:33:36 +0530

    Readme.md update

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,6 @@
 export const APP_LOGO_URL="./mockImages/RGB_Logo_72ppi.png"
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/"
+export const REST_CARDS="https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 export const Deals="https://media-assets.swiggy.com/swiggy/image/upload/"
 export const toppicksImg="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/"
 export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=29.9680035&lng=77.55520659999999&restaurantId="
